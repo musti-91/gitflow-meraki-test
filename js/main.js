b2a6b13a77c357c7f1f0ce0c8bb883c1532326e9
sayed nazif <nazif198805@gmail.com>
2018-04-19 14:59:37 +0200

    add login form

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function loginPage() {
+  let html = "";
+  html += `<form>
+    <h1> It is made by one and only naser </h1>
+    <div>
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+  
+      <label><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+  
+      <button type="submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+    </div>
+  
+    <div style="background-color:#f1f1f1">
+      <button type="button">Cancel</button>
+      <span >Forgot <a href="#">password?</a></span>
+    </div>
+  </form>`;
+
+  document.getElementById("nazif").insertAdjacentHTML("afterbegin", html);
+}
+
+loginPage();
